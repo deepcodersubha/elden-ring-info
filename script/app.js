0b1aa78cee8de59def941b23fb995e02a02eaf1b
Subhadeep Mukherjee <69961498+deepcodersubha@users.noreply.github.com>
2024-09-18 21:33:37 +0530

    Update app.js

--- script/app.js
+++ script/app.js
@@ -22,9 +22,19 @@ const menu = [
 ];
 
 for (const menus of menu) {
-  menuItemContent.innerHTML += `<li><a>${menus.title}</a></li>`;
+  let endpoint = menus.title.split(" ")[0].toLowerCase();
 
-  fetch(`${BASE_URL}/${menus.title.toLowerCase()}`)
+  menuItemContent.innerHTML += `<li><a class="menuItems">${menus.title}</a></li>`;
+
+  const menuItems = document.querySelectorAll(".menuItems");
+
+  menuItems.forEach((element) => {
+    element.addEventListener("click", () => {
+      console.log(element.textContent);
+    });
+  });
+
+  fetch(`${BASE_URL}/${endpoint}`)
     .then((response) => {
       if (response.status >= 200 && response.status <= 300) {
         return response.json();
@@ -34,13 +44,24 @@ for (const menus of menu) {
     })
     .then((data) => {
       data.data.forEach((items) => {
-        const cardHtml = `<div class="card bg-base-100 w-96 shadow-xl">
-                <figure class="w-full h-[15rem] object-contain mx-auto">
-                    <img class="object-cover" src=${items.image} alt="Item Image" />
+        const readMore = "<a class='readMore text-blue-500'>...Read More</a>";
+
+        const shortenedDescription =
+          items.description?.length > 100
+            ? items.description.substring(0, 100) + readMore
+            : items.description;
+
+        const cardHtml = `<div class="card bg-base-100 w-96 h-[400px] shadow-xl">
+                <figure>
+                    <img class="h-[200px] w-full object-contain" src=${
+                      items.image
+                        ? items.image
+                        : "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+                    } alt="Item Image" />
                 </figure>
-                <div class="card-body">
+                <div class="card-body h-20">
                     <h2 class="card-title">${items.name}</h2>
-                    <p>${items.description}</p>
+                    <p>${shortenedDescription}</p>
                 </div>
             </div>`;
 
